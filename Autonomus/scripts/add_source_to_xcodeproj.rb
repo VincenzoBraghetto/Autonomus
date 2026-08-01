a69b8ef7cfd8bc1ff2c6124754a854db94a58d60
Vincenzo Braghetto <vabraghetto@miuandes.cl>
2026-08-01 18:35:56 -0400

    testing ruby script for xcode

--- Autonomus/scripts/add_source_to_xcodeproj.rb
+++ Autonomus/scripts/add_source_to_xcodeproj.rb
@@ -1,23 +1,24 @@
 require 'xcodeproj'
 
-FILE_TO_ADD = 'Autonomus/click.cpp'
+FILE_TO_ADD = 'click.cpp'
 puts "STARTED RUBY SCRIPT"
 
-Dir.glob('Autonomus-macOS.xcodeproj').each do |proj_path|
-  project = Xcodeproj::Project.open(proj_path)
-  group = project.main_group.find_subpath('sources', true)
 
-  already_present = group.files.any? { |f| f.path == 'click.cpp' }
+proj_path = "projects/Autonomus-macOS.xcodeproj"
 
-  if already_present
+project = Xcodeproj::Project.open(proj_path)
+group = project.main_group.find_subpath('sources', true)
+
+already_present = group.files.any? { |f| f.path == 'click.cpp' }
+
+if already_present
     puts "click.cpp ya está en #{proj_path}, salteando."
-  else
+else
     file_ref = group.new_file(FILE_TO_ADD)
     project.targets.each do |target|
-      next unless target.respond_to?(:source_build_phase)
-      target.source_build_phase.add_file_reference(file_ref)
+        next unless target.respond_to?(:source_build_phase)
+        target.source_build_phase.add_file_reference(file_ref)
     end
-    project.save
-    puts "Agregado click.cpp a #{proj_path}."
-  end
+project.save
+puts "Agregado click.cpp a #{proj_path}."
 end
